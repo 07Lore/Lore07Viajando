cd61933aff19796b940f27a09bc45ed1791e2698
07Lore <moveupcontacto10@gmail.com>
2025-09-11 10:35:57 -0300

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -7,7 +7,7 @@ export default function Loader({ text = "Buscando..." }) {
         <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" fill="none"></circle>
         <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
       </svg>
-      <div className="text-lg font-medium text-gray-700">{text}</div>
+      <div className="text-lg font-medium text-pastelChampagne">{text}</div>
     </div>
   );
 }
