665483c695c68af052b26821996974086cce955f
07Lore <moveupcontacto10@gmail.com>
2025-09-13 16:22:38 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
- content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-],
-
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx,html}",
+    "./public/index.html"
+  ],
   theme: {
     extend: {
       colors: {
-        loreverde: "#00B894",     // Verde personalizado
-        champagne: #F7E7CE",     // Champagne
-        naranja: "#FF6F00",       // Naranja
-        grisoscuro: "#1E1E1E",    // Gris oscuro
-      },
-    },
+        loreverde: "#00B894",
+        champagne: "#F7E7CE",
+        naranja: "#FF6F00",
+        grisoscuro: "#1E1E1E"
+      }
+    }
   },
   plugins: [],
 };
-
